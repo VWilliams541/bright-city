0fefbb317a1340b0d40c091cc208152d8e102307
LtCupcakes541 <vinnywilliams13@gmail.com>
2018-07-26 15:45:42 -0400

    add small styling change and about page

--- src/pages/leaders.js
+++ src/pages/leaders.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import '../styles/leaders.scss';
 
 export default () =>
-<div>
-    <h1>About Us</h1>
-    <p></p>
+<div className="leaders">
+    <div className="image">
+        <img src="http://brightcitychurch.com/wp-content/uploads/2018/03/BK5B9310.jpg" alt="Ike and Sharon" />
+    </div>
+    <div className="text">
+    <h1>About Our Leaders</h1>
+    <p>For years, Ike and Sharon Miller have sensed God calling them to lead together,
+        and they believe God was equipping them specifically for Raleigh-Durham. In addition to their years of ministry experience, 
+        both earned Master of Divinity degrees from Duke Divinity School and PhDs from Trinity Evangelical Divinity School. 
+        Together, they have a passion for making the deep things of faith accessible to everyone, no matter where they are in their faith.
+        Ike loves Duke basketball, good food, friends, and coffee. Sharon is an author and a speaker,
+        with a big heart for teaching and encouraging women. She also blames Ike for her addiction to coffee. 
+        They have two boys, Isaac and Coen, and they recently welcomed their first little girl, Sadie, who is in charge now.
+    </p>
+    </div>
 </div>
